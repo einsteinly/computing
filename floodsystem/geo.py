905ccabbaaaba9f4c0f60c95c46805325a8ff0ae
einsteinly <lieinsteinxi@gmail.com>
2017-01-29 21:39:47 +0000

    added stations_within_radius function and tests

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -8,13 +8,27 @@ from haversine import haversine
 from .stationdata import build_station_list
 
 def stations_by_distance(stations, p):
-	"""This function takes two arguments: a list of stations and a coordinate p,
-	and returns a sorted list of (station, distance) tuples, 
-	where distance (float) is the distance of the station (MonitoringStation) from the coordinate p
-	"""
-	sorted_stations = [];
+    """This function takes two arguments: a list of stations and a coordinate p,
+    and returns a sorted list of (station, distance) tuples, 
+    where distance (float) is the distance of the station (MonitoringStation) from the coordinate p
+    """
+    sorted_stations = [];
 
-	for station in stations:
-		sorted_stations.append( (station, haversine(p, station.coord)) );
+    for station in stations:
+        sorted_stations.append( (station, haversine(p, station.coord)) );
 
-	return sorted_by_key(sorted_stations,1);
+    return sorted_by_key(sorted_stations,1);
+
+
+def stations_within_radius(stations, centre, radius):
+    """
+        Arguments: list of stations, coordinate of the centre - a tuple (x,y), and the required radius in km
+        This function returns a list of stations that are within a radius 'radius' from coordinate 'centre'
+    """
+
+    sorted_stations = [];
+
+    # get a list of stations with their respective stations from the centre
+    sorted_stations = stations_by_distance(stations, centre);
+
+    return [ station for station in sorted_stations if station[1] <= radius ];
--- tests/test_geo.py
+++ tests/test_geo.py
@@ -1,7 +1,7 @@
 """Unit test for geo module"""
 
 import pytest
-from floodsystem.geo import stations_by_distance
+import floodsystem.geo as geo
 from floodsystem.stationdata import build_station_list
 
 def test_stations_by_distance():
@@ -9,10 +9,17 @@ def test_stations_by_distance():
     cam_coor = (52.2053, 0.1218);
 
     stations = build_station_list();
-    sorted_stations = stations_by_distance(stations, cam_coor);
+    sorted_stations = geo.stations_by_distance(stations, cam_coor);
 
     #Test that the function stations_by_distance runs
     assert sorted_stations;
 
     #Test that a list is returned
-    assert 	isinstance(sorted_stations, list);
+    assert  isinstance(sorted_stations, list);
+
+    #Test that stations_within_radius function runs without error
+    assert geo.stations_within_radius(stations, cam_coor, 10);
+
+    #Test that stations_within_radius function returns a list
+    assert  isinstance(geo.stations_within_radius(stations, cam_coor, 10), list);
+    
