835529fbc2f41eff6983d9bce1f49252e04b1b5b
einsteinly <lieinsteinxi@gmail.com>
2017-01-29 21:50:08 +0000

    improved test file to include tests of instances

--- tests/test_geo.py
+++ tests/test_geo.py
@@ -3,12 +3,15 @@
 import pytest
 import floodsystem.geo as geo
 from floodsystem.stationdata import build_station_list
+from floodsystem.station import MonitoringStation
+
+
+cam_coor = (52.2053, 0.1218);
+stations = build_station_list();
 
 def test_stations_by_distance():
     
-    cam_coor = (52.2053, 0.1218);
 
-    stations = build_station_list();
     sorted_stations = geo.stations_by_distance(stations, cam_coor);
 
     #Test that the function stations_by_distance runs
@@ -17,9 +20,14 @@ def test_stations_by_distance():
     #Test that a list is returned
     assert  isinstance(sorted_stations, list);
 
+
+def test_stations_within_radius():
     #Test that stations_within_radius function runs without error
     assert geo.stations_within_radius(stations, cam_coor, 10);
 
     #Test that stations_within_radius function returns a list
-    assert  isinstance(geo.stations_within_radius(stations, cam_coor, 10), list);
-    
+    assert isinstance(geo.stations_within_radius(stations, cam_coor, 10), list);
+
+    #Test that items inside the list are instances of MonitoringStation
+    for station in geo.stations_within_radius(stations, cam_coor, 10):
+        assert isinstance(station, MonitoringStation)
