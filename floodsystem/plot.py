354a428d80d069d01d62701a83cb0bf50d67470c
einsteinly <xl404@tw904.eng.cam.ac.uk>
2017-02-02 13:06:22 +0000

    added plotting functions and tests

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -4,17 +4,19 @@ import plotly
 import plotly.graph_objs as go
 
 
-def plot_water_levels(station, dates, levels):
+def plot_water_levels(stations, dates, levels):
 	'''
 		displays a plot of the water level data against time for a station, and include on the plot lines for the typical low and high levels
-		arguments: station, dates: array of date objects, levels: array of water levels
+		arguments: list of stations, dates: list of arrays of date objects, levels: list of arrays of water levels
 	'''
-	x = dates;
-	data = [go.Scatter(x=x,y=levels)];
-	layout = dict( title=station.name,
-		xaxis = dict(title='dates'),
-		yaxis = dict(title='water level')
-		 );
-	plotly.offline.plot(dict(data=data,layout=layout), filename='water levels plot.html');
 
-	return True;
+	data = [];
+
+	for key, station in enumerate(stations):
+		data.append(go.Scatter(x=dates[key],y=levels[key], name=station.name) );
+	layout = dict( title = 'Water Levels Plot',
+		xaxis = dict(title='Dates'),
+		yaxis = dict(title='Water level'),
+		showlegend = True);
+	
+	return plotly.offline.plot(dict(data=data,layout=layout), filename='water levels plot.html');
--- tests/test_plot.py
+++ tests/test_plot.py
@@ -18,9 +18,11 @@ for station in stations:
 
 # Fetch data over past 10 days
 dt = 10
-dates, levels = datafetcher.fetch_measure_levels(station_cam.measure_id,
+dates = [[]];
+levels = [[]];
+dates[0], levels[0] = datafetcher.fetch_measure_levels(station_cam.measure_id,
                                      dt=datetime.timedelta(days=dt))
 
 def test_plot_water_levels():
 	#Test that the function runs without error to give the required plot
-	assert plot.plot_water_levels(station_cam, dates, levels);
+	assert plot.plot_water_levels([station_cam], dates, levels);
