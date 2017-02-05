df67d8777ea0a47e8bd1d35dc46439ad56e1bb6e
Shengyuan Zhang <sz346@cam.ac.uk>
2017-02-05 13:40:29 +0000

    Complete tast 1F

---
+++ Task1F.py
@@ -0,0 +1,15 @@
+import floodsystem.station as station
+def run():
+    """Requirements for Task 1F"""
+
+    # Build list of stations
+    stations = stationdata.build_station_list()
+
+    print(station.inconsistent_typical_range_stations(stations))
+
+    
+if __name__ == "__main__":
+    print("*** Task 1F: CUED Part IA Flood Warning System ***")
+
+    # Run Task1F
+    run()
